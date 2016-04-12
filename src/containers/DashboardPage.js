cf266b04e6284b25cc87c57a3877d1750ed4f53c
francis <cloud9+cifren8x@gmail.com>
2016-04-12 13:59:53 +0000

    add link on dashboard

--- src/containers/DashboardPage.js
+++ src/containers/DashboardPage.js
@@ -5,6 +5,10 @@ export default class DashboardPage extends React.Component {
     return (
       <div>
           <h1>Sound admin</h1>
+          <div>
+            <p>Find the repository <a href="https://github.com/cifren/sound-admin-deploy" target="_blank">for this website here</a></p>
+            <p>And its sources <a href="https://github.com/cifren/sound-admin" target="_blank">Sound-Admin</a></p>
+          </div>
       </div>
     );
   }
