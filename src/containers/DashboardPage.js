7929641a6685e73d3c6e92ca3ea3dca0796bb272
francis <cifren@msn.com>
2016-04-20 11:30:48 +0200

    Update DashboardPage.js

--- src/containers/DashboardPage.js
+++ src/containers/DashboardPage.js
@@ -6,6 +6,7 @@ export default class DashboardPage extends React.Component {
       <div>
           <h1>Sound admin</h1>
           <div>
+            <h3>Some slow load can occure on the very first page loading, the server must recover from sleep sometimes</h3>
             <p>Find the repository <a href="https://github.com/cifren/sound-admin-deploy" target="_blank">for this website here</a></p>
             <p>Find the source of this demo <a href="https://github.com/cifren/sound-admin" target="_blank">Demo Sound-Admin</a></p>
             <p>Find the Repo of Sound-Admin <a href="https://github.com/cifren/sound-admin" target="_blank">Sound-Admin</a></p>
